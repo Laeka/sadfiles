d0bd56c0cbcca9452a5f36cf2a7e1f079940fc78
Laeka <ale.tct16@gmail.com>
2023-05-09 10:41:39 -0500

    new desktop work

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -5,9 +5,9 @@ killall picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 picom --config ~/.config/picom/picom.conf -b &
 
-# xinput disable 19 &
+# xinput disable 12 &
 
-# ~/.config/polybar/launch.sh &
+~/.config/polybar/launch.sh &
 ~/.fehbg &
 dunst &
 autotiling &
@@ -17,4 +17,4 @@ sxhkd -c $HOME/.config/i3/sxhkd/sxhkdrc &
 
 # laptop monitor off is external monitor is connected
 #
-xrandr | grep 'HDMI1 connected' > /dev/null && xrandr --output eDP1 --off & 
+#xrandr | grep 'HDMI1 connected' > /dev/null && xrandr --output eDP1 --off & 
